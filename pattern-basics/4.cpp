563cfc573f5b50bd00f1d39303cf23c810e5c2e7
akshay1027 <akshayar1027@gmail.com>
2021-02-19 03:49:57 +0530

    pattern questions

---
+++ pattern-basics/4.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            if(j<=4-i)
+            {
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
+
+
+/*
+
+*****
+**** 
+***  
+**   
+* 
+
+*/
